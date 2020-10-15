280aea6d2ca7875e1a11498a6233cb9e6ed4c779
TAYAGCH <tayagch@oocl.com>
2020-10-15 22:01:05 +0800

    [MarsRover][Christian] test : implement case when input 0,0 N A then output CommandNotDefinedException

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -3,6 +3,7 @@ package com.afs.tdd;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MarsRoverTest {
     // 0,0 N M
@@ -160,5 +161,17 @@ class MarsRoverTest {
         assertEquals(0,marsRover.getLocationX());
         assertEquals(1,marsRover.getLocationY());
         assertEquals("W",marsRover.getHeading());
+    }
+    
+    @Test
+    public void should_return_illegalArgumentException_given_execute_command_x_0_y_0_heading_N_when_command_A(){
+        // Given
+        MarsRover marsRover = new MarsRover(0,0,"N","A");
+
+        // When
+        Exception exception = assertThrows(IllegalAccessException.class, () -> marsRover.executeCommands());
+
+        // Then
+        assertEquals("CommandNotDefinedException",exception.getMessage());
     }
 }

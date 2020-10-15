955a6c47f0c4693fe2c9bb9cbd0f4a2316794ff8
TAYAGCH <tayagch@oocl.com>
2020-10-15 21:30:41 +0800

    [MarsRover][Christian] test : implement case when input 0,0 S L and then output 0,0 E

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -53,4 +53,16 @@ class MarsRoverTest {
         assertEquals(-1,marsRover.getLocationY());
         assertEquals("S",marsRover.getHeading());
     }
+
+    @Test
+    void should_x_0_y_0_heading_E_given_execute_command_x_0_y_0_heading_S_when_command_L() {
+        // Given
+        MarsRover marsRover = new MarsRover(0,0,"S","L");
+        // When
+        marsRover.executeCommands();
+        // Then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(0,marsRover.getLocationY());
+        assertEquals("E",marsRover.getHeading());
+    }
 }

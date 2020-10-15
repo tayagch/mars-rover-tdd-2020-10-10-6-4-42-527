0cfaa23f38e78dffbe4f2a0635f10a5ff925b6d9
TAYAGCH <tayagch@oocl.com>
2020-10-15 17:08:59 +0800

    [MarsRover][Christian] test : implement case when input command 0,0 N M then output 0,1 N

---
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -0,0 +1,21 @@
+package com.afs.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MarsRoverTest {
+    // 0,0 N M
+    @Test
+    void should_x_0_y_1_heading_N_given_execute_command_when_command_M() {
+        // Given
+        MarsRover marsRover = new MarsRover(0,0,"N","M");
+        // When
+        marsRover.executeCommands();
+        // Then
+        assertEquals(0,marsRover.getLocationX());
+        assertEquals(1,marsRover.getLocationY());
+        assertEquals("N",marsRover.getHeading());
+    }
+
+}
